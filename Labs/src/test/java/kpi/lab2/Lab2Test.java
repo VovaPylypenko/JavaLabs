eb67084de1acf6d939618afd5281e611644da571
Volodia Pylypenko <Pylypenko199813@gmail.com>
2018-10-15 21:07:04 +0300

    lab 2 - UT fix

--- Labs/src/test/java/kpi/lab2/Lab2Test.java
+++ Labs/src/test/java/kpi/lab2/Lab2Test.java
@@ -4,12 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Lab2Test {
 
-    final String testText      = "Hello, 11.30o`clock. lab 2";
-    final double expectedCount = 13.3;
+    private final String TEST_TEXT     = "Hello, 11.30o`clock lab 2";
+    private final double EXPECTED_COUNT = 13.3;
 
     @org.junit.jupiter.api.Test
     void findAndAddAllNumbers() {
-        Lab2 lab2Task = new Lab2();
-        assertEquals(expectedCount, lab2Task.findAndAddAllNumbers(testText));
+        NumberReader numberReader = new NumberReader();
+        assertEquals(EXPECTED_COUNT, numberReader.findAndAddAllNumbers(TEST_TEXT));
     }
 }
